37afc5cb50f453981ff15e42f0fc5e4a7ec3ea45
Renan Gregório <145120433+RenanGreg@users.noreply.github.com>
2025-02-25 09:49:58 -0300

    Create exercicio_2.java

---
+++ exercicio_2.java
@@ -0,0 +1,56 @@
+public class exercicio2 {
+    
+    public static void main (String[] args) throws Exception{ 
+        
+        int A = 20; 
+        int B = 4; 
+        int C = 11; 
+        int D = 7; 
+        int E = 1; 
+        int F = 13; 
+
+        System.out.println(A < B); 
+
+        System.out.println(C == 5); 
+
+        System.out.println(A == C); 
+
+        System.out.println(D != F); 
+
+        System.out.println(A == (F + D)); 
+
+        System.out.println(B <= E); 
+
+        System.out.println(C < A); 
+
+        System.out.println(B == 4); 
+
+        System.out.println(E <= 25); 
+
+        System.out.println((E + F) == (B + C -1)); 
+
+        System.out.println(C * E >= 10); 
+
+        System.out.println(B * D <= 16); 
+
+        System.out.println((A + 16 * B) >= (A/B - 2)); 
+
+        System.out.println(21 / D == 3 ); 
+
+        System.out.println((F + C * D - 10) >= (F - A * E + 40)); 
+
+        System.out.println((B * D + A ) != (B * D + 20)); 
+
+        System.out.println((50 - A * E - B + 5) < (C + 8 * C - 5)); 
+
+        System.out.println(A * (33 * 10) == A * (30 * 11)); 
+
+        System.out.println((42 / B + 125) > (167 / A - 125)); 
+
+        System.out.println((657 * D + F / A) <= (412 - A * 557 + 4)); 
+
+        System.out.println((85 / D - 64 / B) >= (85 * D - 64 / B));
+
+
+    }
+}
