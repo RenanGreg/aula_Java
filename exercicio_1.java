ec2bc27088454bf2d479ac29644549158d25182c
Renan Gregório <145120433+RenanGreg@users.noreply.github.com>
2025-02-25 09:49:04 -0300

    Create exercicio_1.java

---
+++ exercicio_1.java
@@ -0,0 +1,24 @@
+ public class exercicio1 {
+ 
+
+    
+    
+    public static void main (String[] args) throws Exception{ 
+    int A = 10; 
+    int B = 20; 
+    int C = 30;  
+
+    int soma = A + B + C;
+    int sub = A - B - C; 
+    int mult = A * B * C; 
+
+    System.out.println("O resultado de soma é:" + soma);  
+    System.out.println("O resultado de subtração é" + sub); 
+    System.out.println("O resultado de multiplicação é:" + mult); 
+
+
+
+
+    
+    }
+}
